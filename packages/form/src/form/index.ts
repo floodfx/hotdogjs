fdce83ad9dda31d304a251980108508365b2b6b8
Donnie Flood <donnie@floodfx.com>
2025-02-16 19:04:22 -0700

    - clean up and document the form helpers - show errors for any fields that have been touched

--- packages/form/src/form/index.ts
+++ packages/form/src/form/index.ts
@@ -1,4 +1,4 @@
-import { SomeZodObject, ZodIssue } from "zod";
+import { SomeZodObject, ZodIssue, type ZodSchema } from "zod";
 
 /**
  * Validation errors keyed by properties of T
@@ -52,14 +52,14 @@ export interface Form<T> {
  * A Form implementation that uses Zod for validation.
  */
 export class ZodForm<T extends object> implements Form<T> {
-  #schema: SomeZodObject;
+  #schema: SomeZodObject | ZodSchema;
   #data: Partial<T>;
   #action?: string;
   #touched: Set<keyof T>;
   #valid: boolean;
   #errors?: FormErrors<T>;
 
-  constructor(schema: SomeZodObject, data?: Partial<T>, action?: string) {
+  constructor(schema: SomeZodObject | ZodSchema, data?: Partial<T>, action?: string) {
     this.#schema = schema;
     this.#data = data ?? {};
     this.#action = action;
@@ -89,13 +89,19 @@ export class ZodForm<T extends object> implements Form<T> {
     this.#action = action;
     this.#data = { ...this.#data, ...newAttrs };
 
+    // check if update is for a specific field
+    const hasTarget = Object.keys(newAttrs).some((key) => key === "_target");
+    const target = (newAttrs as any)["_target"] as keyof FormErrors<T> | undefined;
+
     // for key in newAttrs update touched
     for (const key in newAttrs) {
+      // skip adding to touched if hasTarget and key is not target
+      if (hasTarget && target !== key) {
+        continue;
+      }
       this.#touched.add(key as keyof T);
     }
 
-    // check if update is for a specific field
-    const target = (newAttrs as any)["_target"] as keyof FormErrors<T> | undefined;
     // handle case where _target is set but the newData does not contain the _target field
     // this happens in the case of a checkbox that is unchecked
     if (target && !newAttrs[target]) {
@@ -120,16 +126,23 @@ export class ZodForm<T extends object> implements Form<T> {
       return;
     }
 
-    // otherwise build the errors mapping
+    // otherwise (we have errors) so build the errors mapping
     this.#errors = result.error.issues.reduce((acc: FormErrors<T>, issue: ZodIssue) => {
+      const path = issue.path[0] as keyof T;
+
       // if no target is present, include all errors
       if (!target) {
         // @ts-ignore
-        acc[issue.path[0]] = issue.message;
+        acc[path] = issue.message;
         return acc;
       }
-      // otherwise only include errors for the target field
-      if (issue.path[0] === target) {
+      // if not target but has been touched, include the error
+      if (this.#touched.has(path)) {
+        acc[path] = issue.message;
+        return acc;
+      }
+      // otherwise only include errors for the target field and fields that are touched
+      if (path === target) {
         acc[target] = issue.message;
         return acc;
       }
--- packages/form/src/tags/inputs.ts
+++ packages/form/src/tags/inputs.ts
@@ -1,24 +1,54 @@
-import { BaseComponent, html, safe } from "hotdogjs";
+import { BaseComponent, html, safe, type Template } from "hotdogjs";
 import type { Form } from "../form";
 
-interface InputOptions {
+/**
+ * Options for the input tag.
+ */
+export interface InputOptions {
+  /**
+   * The placeholder to use for the input.
+   */
   placeholder?: string;
+  /**
+   * The autocomplete to use for the input.
+   */
   autocomplete?: "off" | "on";
+  /**
+   * The debounce to use for the input.
+   */
   debounce?: number | "blur" | "focus";
-  type?: "text" | "tel";
-  className?: string;
+  /**
+   * The type to use for the input.
+   */
+  type?: "text" | "tel" | "password";
+  /**
+   * The classes to use for the input.
+   */
+  classes?: string;
 }
 
-export const text_input = <T>(form: Form<T>, key: keyof T, options?: InputOptions) => {
+/**
+ * text_input is a tag that creates a text input element `Template`.
+ *
+ * @param form - The form to use for the input.
+ * @param key - The key to use for the input.
+ * @param options - The options for the input.
+ * @returns The text input `Template`.
+ */
+export const text_input = <T>(form: Form<T>, key: keyof T, options?: InputOptions): Template => {
   const placeholder = options?.placeholder ? safe(` placeholder="${options.placeholder}"`) : "";
   const autocomplete = options?.autocomplete ? safe(` autocomplete="${options.autocomplete}"`) : "";
   const debounce = options?.debounce ? safe(` hd-debounce="${options.debounce}"`) : "";
-  const className = options?.className ? safe(` class="${options.className}"`) : "";
+  const classes = options?.classes ? safe(` class="${options.classes}"`) : "";
   const type = options?.type ?? "text";
   const id = `input_${String(key)}`;
   const value = form.data[key] ?? "";
-  // prettier-ignore
-  return html`<input type="${type}" id="${id}" name="${String(key)}" value="${value}"${className}${autocomplete}${placeholder}${debounce}/>`;
+  return html`<input
+    type="${type}"
+    id="${id}"
+    name="${String(key)}"
+    value="${value}"
+    ${classes}${autocomplete}${placeholder}${debounce} />`;
 };
 
 export class TextInput<T> extends BaseComponent {
@@ -52,7 +82,7 @@ interface ErrorTagOptions {
 export const error_tag = <T>(form: Form<T>, key: keyof T, options?: ErrorTagOptions) => {
   const error = form.errors ? form.errors[key] : undefined;
   if (!form.valid && error) {
-    const className = options?.className ?? "invalid-feedback";
+    const className = options?.className ?? "text-sm text-error";
     return html`<span class="${className}" hd-feedback-for="${key}">${error}</span>`;
   }
   return html``;
